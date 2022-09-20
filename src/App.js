d6d9b4ad46806ec51cf12d2b353344c10e8cb6e6
gabrielgusso <gabriel.gusso@hotmail.com>
2022-09-20 01:50:21 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+export default function App(){
+    return(
+        <main>
+            <div>
+                <img src='assets/forca0.png' alt='forca'/>
+                <button>Escolha a palavra</button>
+                <input type="submit"/>
+            </div>
+        </main>
+    )
+}
